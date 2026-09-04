ebe1f3cff6ce03990ae16ba02ed5a274f78a756d
fernandoJs251 <silesfercho456@gmail.com>
2026-09-04 08:19:40 -0400

    Configurar GitHub Actions y astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind'; // Si tienes tailwind o alguna otra integración, déjala como está
 
 // https://astro.build/config
 export default defineConfig({
-  server: {
-    port: 3000,
-    host: true
-  }
+  site: 'https://fernandoJs251.github.io',
+  base: '/astro-antigravity-portfolio',
+  integrations: [tailwind()], // Mantén aquí las integraciones que ya tenías
 });
